0f984775531fd87fcf825f1f9142cd8cc26061c4
ShanmukhShashankh <vemula.shanmukhshashankh@gmail.com>
2021-12-19 02:48:13 +0530

    Added a new file

---
+++ 236A-Boy_or_Girl.py
@@ -0,0 +1,9 @@
+username = input()
+username_final = []
+for i in username:
+    if i not in username_final:
+        username_final.append(i)
+if len(username_final)%2==0:
+    print("CHAT WITH HER!")
+else:
+    print("IGNORE HIM!")
