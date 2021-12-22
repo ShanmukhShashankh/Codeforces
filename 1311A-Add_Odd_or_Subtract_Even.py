789ba48cb7082dc025f58fe4958e1ff4c908a60f
ShanmukhShashankh <vemula.shanmukhshashankh@gmail.com>
2021-12-23 01:35:20 +0530

    Created new file

---
+++ 1311A-Add_Odd_or_Subtract_Even.py
@@ -0,0 +1,22 @@
+testcases = int(input())
+while testcases != 0:
+    a,b=input().split()
+    a = int(a)
+    b = int(b)
+    cases = 0
+    if a>b or a==b:
+        diff = a-b
+        if diff%2==0 and diff !=0:
+            cases = 1
+        elif diff==0:
+            cases = 0
+        else:
+            cases = 2
+    elif a<b:
+        diff = b-a
+        if diff%2==0:
+            cases = 2
+        else:
+            cases = 1
+    print(cases)
+    testcases -= 1
