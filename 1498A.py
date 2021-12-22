54ec9d120e03b4966fa9d6949e0f676b40a67e84
ShanmukhShashankh <vemula.shanmukhshashankh@gmail.com>
2021-12-23 00:57:00 +0530

    Added a new file

--- 1498A.py
+++ 1498A.py
@@ -3,7 +3,16 @@ from math import gcd
 testcases = int(input())
 
 while testcases!=0:
-  x = int(input())
-  
-  if gcd()
+  x = input()
+  sum_x = 0
+  for i in x:
+    sum_x += int(i)
+  x = int(x)
+  while gcd(x,sum_x)==1:
+    x+=1
+    sum_x = 0
+    for i in str(x):
+      sum_x += int(i)
+  else:
+    print(x)
   testcases -= 1
