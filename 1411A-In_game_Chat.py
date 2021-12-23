015138cca8c6d32909d089585d0a39879ffe055f
ShanmukhShashankh <vemula.shanmukhshashankh@gmail.com>
2021-12-23 17:41:56 +0530

    Created a new file

---
+++ 1411A-In_game_Chat.py
@@ -0,0 +1,20 @@
+testcases = int(input())
+while testcases!=0:
+    string_len = int(input())
+    string = input()
+    count_parantheses = 0
+    count_letters = 0
+    for i in reversed(range(string_len)):
+        if string[i]==")" and string[i-1]==")":
+            count_parantheses += 1
+        else:
+            break
+    if string_len!=1:
+        count_parantheses+=1
+    count_letters = string_len - count_parantheses
+    
+    if count_parantheses>count_letters:
+        print("Yes")
+    else:
+        print("No")
+    testcases -= 1
