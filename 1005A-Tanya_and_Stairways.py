44f3d7bdd97eea1096ddfbe73e7f67427fd10fc1
ShanmukhShashankh <vemula.shanmukhshashankh@gmail.com>
2021-12-23 14:05:45 +0530

    New file

---
+++ 1005A-Tanya_and_Stairways.py
@@ -0,0 +1,21 @@
+x = int(input())
+stairs = input().split()
+stairways=0
+stairs_int = []
+max_steps = []
+
+for i in stairs:
+    stairs_int.append(int(i))
+
+for i in stairs:
+    if int(i) == 1:
+        stairways +=1
+
+for i in range(1, len(stairs_int)):
+    if stairs_int[i] == 1:
+        max_steps.append(stairs_int[i-1])
+
+max_steps.append(stairs_int[-1])
+
+print(stairways)
+print(*max_steps, sep=' ')
