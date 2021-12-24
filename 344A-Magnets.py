bb50d4918922fc349bdfc22678479c82920b86d0
ShanmukhShashankh <vemula.shanmukhshashankh@gmail.com>
2021-12-25 02:17:34 +0530

    New File

---
+++ 344A-Magnets.py
@@ -0,0 +1,15 @@
+magnets = int(input())
+arrangement = []
+groups = 0
+for i in range(magnets):
+    x = int(input())
+    arrangement.append(x)
+
+for i in range(1, magnets):
+    if arrangement[i-1] != arrangement[i]:
+        groups += 1
+        
+groups += 1
+print(groups)
+    
+    
