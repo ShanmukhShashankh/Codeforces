7ed21b87e2a7d9a1e3133ff2bd509453ec5620f9
ShanmukhShashankh <vemula.shanmukhshashankh@gmail.com>
2021-12-25 01:49:21 +0530

    New file

---
+++ 1421A-XORwice.py
@@ -0,0 +1,9 @@
+testcases = int(input())
+while testcases!=0:
+    a,b = input().split()
+    a = int(a)
+    b = int(b)
+    print(a^b)
+    testcases -= 1
+
+#https://codeforces.com/problemset/problem/1421/A
