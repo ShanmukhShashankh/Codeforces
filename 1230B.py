408edd5a1a84fb9100119995e266a3cb6b645d15
ShanmukhShashankh <vemula.shanmukhshashankh@gmail.com>
2021-12-25 18:43:04 +0530

    NEW FILE

---
+++ 1230B.py
@@ -0,0 +1,9 @@
+length, no_of_changes = input().split()
+length, no_of_changes = int(length), int(no_of_changes)
+number = int(input())
+
+if length!=0:
+    for i in range(no_of_changes):
+        
+
+#https://codeforces.com/problemset/problem/1230/B
