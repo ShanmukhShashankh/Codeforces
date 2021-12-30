1edf9fe7c5ec2771b0d1203b0907e51164f9f173
ShanmukhShashankh <vemula.shanmukhshashankh@gmail.com>
2021-12-31 03:26:09 +0530

    Update

--- 1096A-Find_Divisible.py
+++ 1096A-Find_Divisible.py
@@ -1,6 +1,6 @@
-    testcases = int(input())
-    while testcases!=0:
-        l,r = input().split()
-        l,r = int(l), int(r)
-        print(l,2*l)
-        testcases -= 1
+testcases = int(input())
+while testcases!=0:
+    l,r = input().split()
+    l,r = int(l), int(r)
+    print(l,2*l)
+    testcases -= 1
